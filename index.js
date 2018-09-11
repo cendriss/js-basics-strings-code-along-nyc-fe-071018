48bcbdcd6fe0add71a19c4e5a7c24c3b9b0c5a0d
Christine Endriss <cendriss@gmail.com>
2018-09-11 19:53:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser ="Christine Endriss";
-const welcomeMessage =  'Welcome to Flatbook,${currentUser}!';
+const welcomeMessage =  'Welcome to Flatbook, ${currentUser}!';
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser +'!';
 const shortGreeting = 'Welcome, C!';
