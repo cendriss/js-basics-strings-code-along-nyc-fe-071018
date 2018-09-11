1a5b6ca61901e4ae2042ed30c8dc1386e84349a2
Christine Endriss <cendriss@gmail.com>
2018-09-11 19:42:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-let currentUser ="Christine Endriss"
+const currentUser ="Christine Endriss"
 let welcomeMessage =  'Welcome to Flatbook, Christine Endriss!'
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, CHRISTINE ENDRISS!'
 let shortGreeting = 'Welcome, C!'
